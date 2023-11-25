e98d635f9f007f2cf90472e1e1bb9b7f6fbccd50
lawrence <ganjeblerence@gmail.com>
2023-11-26 02:05:45 +0530

    added binary search algorithm to serarch.py

--- searching/search.py
+++ searching/search.py
@@ -2,7 +2,8 @@
 
 # linear search
 
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+steps = 0
 
 def linear_search(numbers,value):
     for i in  range(len(numbers)):
@@ -13,3 +14,33 @@ def linear_search(numbers,value):
     print(" number not found ")
 
 linear_search(numbers,12)
+
+# binary search 0(logn)
+
+def binary_search(numbers, value):
+    start = 0
+    end = len(numbers)-1
+    middle = end//2
+    
+# the loop should run so long as the start index is less than the middle index
+    while start <= middle:
+        global steps
+        steps = steps + 1
+        print(f"start: {start}, middle: {middle}, end: {end}")
+        print(f"step: {steps}")
+       
+        #check for the value
+        if numbers[middle] == value:
+            print(f"number found at index {middle}")
+            return
+        elif numbers[middle] < value:
+            start = middle + 1
+            middle = (end + start)//2
+        else:
+            end = middle -1
+            middle = (end + start)//2
+
+    print("the number you are looking for was not found ")
+
+
+binary_search(numbers,5)
